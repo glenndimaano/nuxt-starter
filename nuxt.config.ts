fa0cb70450711d169c472a31739ee68cba9f1d99
glenn <grdimaano18@gmail.com>
2024-11-13 12:27:13 +0800

    compatibilityVersion: 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
+  runtimeConfig: {
+    database: {
+      url: process.env.NUXT_DATABASE_URL
+    },
+    session: {
+      secret: process.env.NUXT_SESSION_SECRET
+    }
+  },
   modules: ['@nuxtjs/tailwindcss', 'radix-vue/nuxt'],
   components: [
     {
